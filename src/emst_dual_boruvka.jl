9df4b09126842241a197575a95bf2c0622d1997a
Casey Primel <casey.primel@gmail.com>
2022-04-13 10:50:00 -0400

    Added/modified documentatio, renaming variables+functions for better readability

--- src/emst_dual_boruvka.jl
+++ src/emst_dual_boruvka.jl
@@ -8,45 +8,46 @@ import Statistics.median
 
 
 """
-  DTBSplit
+    Edge(a, b[, w])
 
-implements split of KD-Tree
+Implements undirected edge with optional weight. Paired vertices are ordered by id number.
+...
+# Fields:
+    - `a`: the id of start vertex 
+    - `b`: the id of end vertex
+    - `w=nothing`: optional weight parameter. 
+...
 """
-struct DTBSplit
-    dim::Int64
-    val::Float64
-    sgn::Bool # 0: smaller, 1 : greater
-end
-
-"""
-  MyE
-
-implements undirected edge
-"""
-struct MyE
+struct Edge
     a::Int64
     b::Int64
     w::Float64
-    MyE(pa::Int64,pb::Int64,w::Float64) = (pa<pb) ? new(pa,pb,w::Float64) : new(pb,pa,w::Float64)
+    Edge(pa::Int64, pb::Int64) = new(pa::Int64, pb::Int64, nothing)
+    Edge(pa::Int64, pb::Int64, w::Float64) = (pa < pb) ? new(pa, pb, w::Float64) : new(pb, pa, w::Float64)
 end
-function isequal(a::MyE,b::MyE)
-    return a.a==b.a && a.b==b.b
+function isequal(a::Edge, b::Edge)
+    return a.a == b.a && a.b == b.b
 end
-function hash(a::MyE)
-    return xor( hash(a.a) , hash(a.b) )
-end
-function to_matrices(ee::Vector{MyE})
-    m::Array{Int64,2} = Array{Int64}(undef,length(ee),2)
-    w::Array{Float64} = Array{Float64}(undef,length(ee))
-    for zi=1:length(ee)
-         m[zi,1] = ee[zi].a
-         m[zi,2] = ee[zi].b
-         w[zi] = ee[zi].w
+
+"""
+    write_edgelist(edges)
+
+Creates (unweighted) edge list and weight list from vector of Edge structs. 
+
+Note: Seperate lists needed since different types.
+"""
+function write_edgelist(ee::Vector{Edge})
+    m::Array{Int64,2} = Array{Int64}(undef, length(ee), 2)
+    w::Array{Float64} = Array{Float64}(undef, length(ee))
+    for zi = 1:length(ee)
+        m[zi, 1] = ee[zi].a
+        m[zi, 2] = ee[zi].b
+        w[zi] = ee[zi].w
     end
     return (m, w)
 end
 
-
+"""
 struct DTBProfiling
     n_comparisons::Int64
     n_break_boxdist::Int64
@@ -55,178 +56,160 @@ end
 function DTBProfiling()
     return DTBProfiling(0,0,0)
 end
-
+"""
 
 """
-  DTBNode
+    KDNode
 
-implements node of a KD tree
+Implements node of a KDtree.
+
+...
+# Fields
+- `id`:
+- `data`: data in 2-dimensional array (row=sample, column=feature)
+- `subset`: indices of samples contained in node
+- `box_lb`: lower bound of node
+- `box_ub`: upper bound of node
+- `dQ`: length of candidate edge
+- `left`: reference to left child node
+- `right`: reference to right child node
+...
 """
-mutable struct DTBNode
+mutable struct KDNode
     id::Int64 # identifies the node ; root has 00..00 , then
-              # levels (right/left) are encoded as 01 or 10
-              # i.e. node root->"right"->"right"->"left" ends with:
-              # 10 01 01 00
-              #  l r  r  R
+    # levels (right/left) are encoded as 01 or 10
+    # i.e. node root->"right"->"right"->"left" ends with:
+    # 10 01 01 00
+    #  l r  r  R
 
-    # data / indidces of this kd tree node
     data::Array{Float64,2}
     subset::Array{Int64}
-    #splits::Array{DTBSplit,1}
 
-    # kd tree node bounds
     box_lb::Array{Float64,1}
     box_ub::Array{Float64,1}
 
-    # max_{v in this node} ( length of candidate edge of v)
     dQ::Float64
 
-    left::DTBNode
-    right::DTBNode
+    left::KDNode
+    right::KDNode
 
-    #DTBNode(data::Array{Float64,2}) = (x = new(data); x.left=x; x.right=x)
-    #function DTBNode(data::Array{Float64,2},subset::Array{Int64},splits::Array{DTBSplit,1},dQ::Float64)
-    function DTBNode(id::Int64,data::Array{Float64,2},subset::Array{Int64},box_lb::Array{Float64,1},box_ub::Array{Float64,1},dQ::Float64)
-        n = new(id,data,subset,box_lb,box_ub,dQ)
-        n.left=n
-        n.right=n
+    function KDNode(id::Int64, data::Array{Float64,2}, subset::Array{Int64}, box_lb::Array{Float64,1}, box_ub::Array{Float64,1}, dQ::Float64)
+        n = new(id, data, subset, box_lb, box_ub, dQ)
+        n.left = n
+        n.right = n
         return n
     end
 end
-function isequal(a::DTBNode,b::DTBNode)
-    return a.id==b.id
+function isequal(a::KDNode, b::KDNode)
+    return a.id == b.id
 end
-function hash(a::DTBNode)
-    #a.id
-    hash(a.id) # we could also drop this hash.. should not matter..
-end
-function is_leaf(n::DTBNode)
-    return n==n
-    #return ( n.id & (1<<63) ) == (1<<63)
+function is_leaf(n::KDNode)
+    return n == n
 end
 
 """
-  kdtree(xx::Array{Float64,2})
+    kdtree(data)
 
-initializes root node of KD tree (but does not compute the splits)
-To build the tree use:
-  kdt_root = kdtree( data )
-  kdtree_split!( kdt_root , 10 ) # 10 is the node size where we stop splitting
+Initializes root node of KD tree
+
+Note:: To build a KD tree:
+            kdt_root = kdtree( data )
+            kdtree_split!( kdt_root , 10 )
 """
 function kdtree(xx::Array{Float64,2})
-    #root = DTBNode(xx,-1,NaN,nothing,nothing)
-    #root = DTBNode(Int64(0),xx,collect(1:size(xx,2)),fill!(ones(size(xx,1)),-Inf),fill!(ones(size(xx,1)),Inf),Inf)
-    root = DTBNode(Int64(0),xx,collect(Int64(1):Int64(size(xx,2))),fill!(ones(size(xx,1)),-Inf),fill!(ones(size(xx,1)),Inf),Inf)
+    root = KDNode(Int64(0), xx, collect(Int64(1):Int64(size(xx, 2))), fill!(ones(size(xx, 1)), -Inf), fill!(ones(size(xx, 1)), Inf), Inf)
     return root
 end
 
 
 """
-  kdtree_split!(node::DTBNode, nmin::Int64)
+    kdtree_split!(node, leafSize)
 
-computes splits of KD tree. nmin indicates the node size where we stop
-splitting nodes.
+Computes splits of KD tree. Stop splitting when node contains number of samples equal to leafSize.
 """
-function kdtree_split!( node::DTBNode, nmin::Int64)
+function kdtree_split!(node::KDNode, nmin::Int64)
 
-    # ensure that parent node is in the tree.
-    #t[node.id] = node
-
-    if( size(node.data,2) <= nmin )
+    if (size(node.data, 2) <= nmin)
         return node
     end
 
-    mind = minimum(node.data,dims=2)
+    mind = minimum(node.data, dims=2)
     maxd = maximum(node.data, dims=2)
     s = maxd - mind
-    # println(s)
-    ds   = findmax(s)
-    # print("$ds")
-    ds   = ds[2][1]
-    #println("$ds")
-    #println(node.data[ds,:])
-    vs   = median(node.data[ds,:])
-    # println("$vs")
-    bx   = node.data[ds,:].<= vs
-    # println(bx)
+    ds = findmax(s)
+    ds = ds[2][1]
+    vs = median(node.data[ds, :])
+    bx = node.data[ds, :] .<= vs
     range_a = node.subset[bx]
     range_b = node.subset[.~bx]
 
-    data_a = node.data[:,bx]
-    data_b = node.data[:,.~bx]
+    data_a = node.data[:, bx]
+    data_b = node.data[:, .~bx]
 
     box_lb_a = copy(node.box_lb)
     box_ub_a = copy(node.box_ub)
     box_ub_a[ds] = vs
     box_lb_b = copy(node.box_lb)
     box_lb_b[ds] = vs
-    box_ub_b     = copy(node.box_ub)
+    box_ub_b = copy(node.box_ub)
 
     id::Int64 = node.id
-    id_depth  = Int( ceil( (64-leading_zeros(id)) / 2) ) + 1 # in this cell we are, i.e. we have to shift id_depth times left by two bits..
-    id_l = id   |  (1)<<(2*id_depth)
+    id_depth = Int(ceil((64 - leading_zeros(id)) / 2)) + 1 # in this cell we are, i.e. we have to shift id_depth times left by two bits..
+    id_l = id | (1) << (2 * id_depth)
     #id_l = id_l |  (1)<<(63)
-    id_r = id   |  (2)<<(2*id_depth)
+    id_r = id | (2) << (2 * id_depth)
     #id_r = id_r |  (1)<<(63)
     #println("id $(id) -> r $(id_l) , l $(id_r)")
 
-    node_left  = kdtree_split!( DTBNode(id_l,data_a,range_a, box_lb_a   , box_ub_a  , Inf) , nmin )
-    node_right = kdtree_split!( DTBNode(id_r,data_b,range_b, box_lb_b   , box_ub_b  , Inf) , nmin )
+    node_left = kdtree_split!(KDNode(id_l, data_a, range_a, box_lb_a, box_ub_a, Inf), nmin)
+    node_right = kdtree_split!(KDNode(id_r, data_b, range_b, box_lb_b, box_ub_b, Inf), nmin)
 
-    node.left  = node_left
+    node.left = node_left
     node.right = node_right
-    #println("SPLIT") #println(data_a) #println(data_b)
-    #println(splits_l)
     return node
 end
 
 """
-  compute_emst(data::Array{Float64,2};nmin::Int64=64)
+    compute_emst(data[,leafSize])
 
 Computes EMST for the given data (where columns are samples).
-nmin is the max number of elements in kd-tree node.
+leafSize is the max number of elements in kd-tree node.
 """
-function compute_emst(data::Array{Float64,2};nmin::Int=64)
-    nmin64 = Int64(nmin)
+function compute_emst(data::Array{Float64,2}; leafSize::Int64=64)
     root = kdtree(data)
-    kdtree_split!(root,nmin64)
-    edges = dtb(root,IntDisjointSets(size(data,2)))
-    return EMST.to_matrices( collect(edges) )
+    kdtree_split!(root, leafSize)
+    edges = dtb(root, IntDisjointSets(size(data, 2)))
+    return EMST.write_edgelist(collect(edges))
 end
 
 
 """
-  dtb(q::DTBNode,e::IntDisjointSets)
+    dtb(q::KDNode,e::IntDisjointSets)
 
-implements the dual-tree Boruvka algorithm which computes the EMST for the
-given KD-tree.
+Implements the dual-tree Boruvka algorithm which computes the Euclidean minimum spanning tree for the given KD-tree.
 """
-function dtb(q::DTBNode,e::IntDisjointSets)
-    edges = Set{MyE}()
+function dtb(q::KDNode, e::IntDisjointSets)
+    edges = Set{Edge}()
 
-    #hashmap_QvsR = Dict{Tuple{DTBNode,DTBNode},Array{Float64,2}}()
-
-    while(e.ngroups>1)
-        ngroups = e.ngroups;
+    while (e.ngroups > 1)
+        ngroups = e.ngroups
         println("--> ngroups: $ngroups")
 
-        # prepare dicts for candidate edges..
+        # Initialize dictionary for candidate edges
         C_dcq = Dict{Int64,Float64}()
-        C_e   = Dict{Int64,MyE}()
-        # init themz
+        C_e = Dict{Int64,Edge}()
         roots = unique(e.parents)
         for ri in roots
             C_dcq[ri] = Inf
         end
 
-        # compute..
-        #fcn(q,q,e,C_dcq,C_e , hashmap_QvsR )
-        fcn(q,q,e,C_dcq,C_e)
+        # Compute component neighbors
+        find_cn(q, q, e, C_dcq, C_e)
 
         # and now add the edges..
-        for ne::MyE in values(C_e)
-            union!(e,ne.a,ne.b)
-            push!(edges,ne)
+        for ne::Edge in values(C_e)
+            union!(e, ne.a, ne.b)
+            push!(edges, ne)
         end
     end
     return edges
@@ -234,84 +217,69 @@ end
 
 
 """
-  fcn(q::DTBNode,r::DTBNode,e::IntDisjointSets , C_dcq::Dict{Int64,Float64} , C_e::Dict{Int64,MyE} )
+    find_cn(
+        q::KDNode,
+        r::KDNode,
+        e::IntDisjointSets, 
+        C_dcq::Dict{Int64,Float64},
+        C_e::Dict{Int64,Edge}
+    )
+
 Find Component Neighbors
 
-Components are identified by the root of the component
-C_dcq : component distances to candidate edges
-C_e   : component candidate edges (comp i candidate edge is C_e[:,i])
+...
+# Arguments:
+- `q`: root of first component
+- `r`: root of second component
+- `e`: union-find datastructure for tracking edges
+- `C_dcq`: Component distances to candate edges
+- `C_e`: Component candidate edges (the candidate edge of component `i` is C_e[:,i])
 """
-function fcn(q::DTBNode,r::DTBNode,e::IntDisjointSets , C_dcq::Dict{Int64,Float64} , C_e::Dict{Int64,MyE} )
-#function fcn(q::DTBNode,r::DTBNode,e::IntDisjointSets , C_dcq::Dict{Int64,Float64} , C_e::Dict{Int64,Tuple{Int64,Int64}} )
-#function fcn(q::DTBNode,r::DTBNode,e::IntDisjointSets , C_dcq::Dict{Int64,Float64} , C_e::Dict{Int64,MyE} , hashmap_QvsR::Dict{Tuple{DTBNode,DTBNode},Array{Float64,2}})
+function find_cn(q::KDNode, r::KDNode, e::IntDisjointSets, C_dcq::Dict{Int64,Float64}, C_e::Dict{Int64,Edge})
 
-    # check all in same component
-    onecomp::Bool  = true
-    joined         = [q.subset;r.subset]
-    #println(joined)
+    # Check that all are in same component
+    onecomp::Bool = true
+    joined = [q.subset; r.subset]
     for ji in joined
-        if(~in_same_set(e,joined[1],ji))
-            onecomp=false
+        if (~in_same_set(e, joined[1], ji))
+            onecomp = false
             break
         end
     end
-    if(onecomp)
+    if (onecomp)
         return
     end
 
-    #check d(Q,R) > d(Q)
-    dqr = computeDQR( q.box_lb,q.box_ub , r.box_lb,r.box_ub )
-    #println("dq : $(dqr) , q.dQ : $(q.dQ)")
-    if( dqr > q.dQ)
-        #println(".")
+    # Check that d(Q,R) > d(Q)
+    dqr = computeDQR(q.box_lb, q.box_ub, r.box_lb, r.box_ub)
+    if (dqr > q.dQ)
         return
     end
 
-    #check if R and Q in a leaf node
-    if( q.left==q && r.left==r ) # that's not very elegant but probably what we have to do?!?
-    #if( is_leaf(q) && is_leaf(r) )
-        #println("LEAF NODES")
-        #n_dQ = -Inf
+    # Check if R and Q in a leaf node
+    if (q.left == q && r.left == r) 
         n_dQ::Float64 = q.dQ
 
-        all_d_qr = Distances.pairwise(Euclidean(),q.data,r.data,dims=2)
+        all_d_qr = Distances.pairwise(Euclidean(), q.data, r.data, dims=2)
 
-        # --> hashing these values does not really speed up things..
-        #     and it uses tons of memory, such that fancier handling of
-        #     hash map entries would be required
-        #if(!haskey(hashmap_QvsR,(q,r)))
-        #    if(!haskey(hashmap_QvsR,(r,q)))
-        #        all_d_qr = Distances.pairwise(Euclidean(),q.data,r.data)
-        #        #hashmap_QvsR[(q,r)] = all_d_qr
-        #    else
-        #        all_d_qr = transpose(hashmap_QvsR[(r,q)])
-        #    end
-        #    hashmap_QvsR[(r,q)] = transpose(all_d_qr)
-        #else
-        #    all_d_qr = hashmap_QvsR[(q,r)]
-        #    #print('.')
-        #end
-
-        for iq = 1:size(q.subset,1)
-            for ir = 1:size(r.subset,1)
+        for iq = 1:size(q.subset, 1)
+            for ir = 1:size(r.subset, 1)
                 qq = q.subset[iq]
                 rr = r.subset[ir]
-                if( in_same_set(e , qq , rr ) )
+                if (in_same_set(e, qq, rr))
                     continue
                 end
 
-                cq = find_root!(e,rr) # compoment of q
+                cq = find_root!(e, rr) # compoment of q
 
                 # check distance:
-                # dist_qr::Float64 = sqrt( sum( (q.data[:,iq]-r.data[:,ir]).^2 ) )
-                dist_qr = all_d_qr[iq,ir]
+                dist_qr = all_d_qr[iq, ir]
 
-                if( dist_qr < C_dcq[ cq ] )
-                    C_dcq[ cq ] = dist_qr
-                    C_e[ cq ]   = MyE(qq,rr, dist_qr) #(qq,rr)
+                if (dist_qr < C_dcq[cq])
+                    C_dcq[cq] = dist_qr
+                    C_e[cq] = Edge(qq, rr, dist_qr) #(qq,rr)
                     # and dQ !
-                    n_dQ = max(n_dQ,dist_qr)
-                    #n_dQ = dist_qr
+                    n_dQ = max(n_dQ, dist_qr)
                 end
             end
         end
@@ -321,16 +289,11 @@ function fcn(q::DTBNode,r::DTBNode,e::IntDisjointSets , C_dcq::Dict{Int64,Float6
     end
 
     # do recursions..
-    #fcn( q.left,r.left,e ,  C_dcq::Dict{Int64,Float64} , C_e::Dict{Int64,Tuple{Int64,Int64}} )
-    #q_left  = is_leaf(q) ? q : t[q.left]
-    #q_right = is_leaf(q) ? q : t[q.right]
-    #r_left  = is_leaf(r) ? r : t[r.left]
-    #r_right = is_leaf(r) ? r : t[r.right]
-    fcn( q.left,r.left    , e ,  C_dcq , C_e ) #, hashmap_QvsR )
-    fcn( q.right,r.left   , e ,  C_dcq , C_e ) #, hashmap_QvsR )
-    fcn( q.left,r.right   , e ,  C_dcq , C_e ) #, hashmap_QvsR )
-    fcn( q.right,r.right  , e ,  C_dcq , C_e ) #, hashmap_QvsR )
-    q.dQ = max( q.left.dQ , q.right.dQ )
+    find_cn(q.left, r.left, e, C_dcq, C_e)
+    find_cn(q.right, r.left, e, C_dcq, C_e)
+    find_cn(q.left, r.right, e, C_dcq, C_e)
+    find_cn(q.right, r.right, e, C_dcq, C_e)
+    q.dQ = max(q.left.dQ, q.right.dQ)
 end
 
 
@@ -340,12 +303,12 @@ end
 compute min dist. between bounding boxes, i.e. between rectangular boxes Q/R with
 bounds given by q_lb/q_ub and r_lb/r_ub
 """
-function computeDQR( q_lb::Array{Float64,1} , q_ub::Array{Float64,1} , r_lb::Array{Float64,1} , r_ub::Array{Float64,1} )
-    d::Int64  = length(q_lb)#size(q_lb,1)
-    rdists::Array{Float64}    = zeros(d)
+function computeDQR(q_lb::Array{Float64,1}, q_ub::Array{Float64,1}, r_lb::Array{Float64,1}, r_ub::Array{Float64,1})
+    d::Int64 = length(q_lb)
+    rdists::Array{Float64} = zeros(d)
     for zd = 1:d
-        rdists[zd] = max(  max( q_lb[zd] - r_ub[zd] , r_lb[zd] - q_ub[zd] ) , 0 )
+        rdists[zd] = max(max(q_lb[zd] - r_ub[zd], r_lb[zd] - q_ub[zd]), 0)
     end
-    dqr::Float64 = sqrt( sum( rdists.^2 ))
+    dqr::Float64 = sqrt(sum(rdists .^ 2))
     return dqr
 end
--- src/emst_validation.jl
+++ src/emst_validation.jl
@@ -6,10 +6,10 @@ using Random
 
 Checks whether the computed EMST contains the nearest neighbor edges.
 """
-function verify_emst(x::Array{Float64,2},e::Array{Int64,2},n_checks::Int)
-    n = min(n_checks,size(x,2))
-    idx_rand = randperm( size(x,2) )[1:n]
-    return verify_emst(x,e, idx_rand )
+function verify_emst(x::Array{Float64,2}, e::Array{Int64,2}, n_checks::Int)
+    n = min(n_checks, size(x, 2))
+    idx_rand = randperm(size(x, 2))[1:n]
+    return verify_emst(x, e, idx_rand)
 end
 
 """
@@ -18,40 +18,48 @@ end
 Checks whether the computed EMST contains the nearest neighbor edges for the
 given indeces.
 """
-function verify_emst(x::Array{Float64,2},e::Array{Int64,2},idx_check::Vector{Int})
+function verify_emst(x::Array{Float64,2}, e::Array{Int64,2}, idx_check::Vector{Int})
     # create edge set
     es::Set{Set{Int64}} = Set{Set{Int64}}()
-    for zi=1:size(e,1); push!(es,Set([e[zi,1],e[zi,2]])); end
+    for zi = 1:size(e, 1)
+        push!(es, Set([e[zi, 1], e[zi, 2]]))
+    end
 
     print("\nVerify: ")
     ok = true
     for zi in 1:length(idx_check)
         ix = idx_check[zi]
-        xd = sum( ( broadcast(-,x[:,ix],x) ).^2 , dims=1 )
+        xd = sum((broadcast(-, x[:, ix], x)) .^ 2, dims=1)
         xd[ix] = Inf
         i_min = findmin(xd)[2]
-        if( sum( xd.==xd[i_min] )>1 )
+        if (sum(xd .== xd[i_min]) > 1)
             println("WARNING: Equidistant vertices discovered..")
-            ok=false
-            iequi = find(any( xd.==xd[i_min] ))
+            ok = false
+            iequi = find(any(xd .== xd[i_min]))
             for iie in iequi
-                ok = isempty( setdiff( Set( [Set([ix,iie])] ) , es ) )
-                if(ok); break; end
+                ok = isempty(setdiff(Set([Set([ix, iie])]), es))
+                if (ok)
+                    break
+                end
             end
-            if(!ok)
+            if (!ok)
                 println("emst kaputt! $(ix) : edge to one of $(iequi) should be in emst!")
-                break;
+                break
             end
         else
             i_min = i_min[1]
-            ok = isempty( setdiff( Set( [Set([ix,i_min])] ) , es ) )
-            if(!ok)
+            ok = isempty(setdiff(Set([Set([ix, i_min])]), es))
+            if (!ok)
                 println("emst kaputt! $(ix) : edge to $(i_min) should be in emst!")
                 break
             end
         end
-        if(mod(zi,ceil(length(idx_check)/10.))==0); print("."); end
+        if (mod(zi, ceil(length(idx_check) / 10.0)) == 0)
+            print(".")
+        end
     end
-    if(ok); print(" ok!\n"); end
+    if (ok)
+        print(" ok!\n")
+    end
     return ok
 end
